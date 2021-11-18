428d26e90ee965c29ba1dfeeb93783f6da7e373c
dimorphic <dimorphic@users.noreply.github.com>
2021-11-18 22:09:35 +0200

    [projects] Fix service to use new DB URL & refactor project mapping

--- app/models/service.projects.js
+++ app/models/service.projects.js
@@ -12,6 +12,13 @@
     //
 
     // TO DO
+    function getUUID() {
+        var fmt = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+        return fmt.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
     
     appService.factory('ProjectsService',
         ['_', '$rootScope', '$q', '$http', 'GENERAL_CONFIG',
@@ -35,47 +42,43 @@
                 var projectList = [];
 
                 // db reference
-                var sync = $http.jsonp(cfg.DBURL);
+                var sync = $http.get(cfg.DBURL);
 
                 // db fetch ok
                 sync.success(function(data) {
-
                     // parse data
-                    angular.forEach(data, function(value, index){
-                        angular.forEach(value.entry, function(classes, index) {
+                    angular.forEach(data, function(item, index){
+                        // project model
+                        var $project = {
+                            id: getUUID(),
+                            year: parseInt(item.year),
 
-                            // project model
-                            var $project = {
-                                id: classes.id.$t,
-                                year: parseInt(classes.gsx$year.$t),
+                            name: item.name,
+                            description: item.description,
+                            client: item.client,
 
-                                name: classes.gsx$name.$t,
-                                description: classes.gsx$caption.$t,
-                                client: classes.gsx$client.$t,
+                            type: item.type,
+                            color: item.color,
 
-                                type: classes.gsx$type.$t,
-                                color: classes.gsx$color.$t,
+                            thumbUrl: item.thumbUrl,
+                            imageUrl: dirImages + item.imageUrl
+                        };
 
-                                thumbUrl: classes.gsx$thumburl.$t,
-                                imageUrl: dirImages + classes.gsx$imageurl.$t
-                            };
+                        // rewrite image source (folder and extension)
 
-                            // rewrite image source (folder and extension)
+                        // if thumb field is 'same'
+                        if($project.thumbUrl === "same") {
+                            $project.thumbUrl = $project.imageUrl;
+                        }
 
-                            // if thumb field is 'same'
-                            if($project.thumbUrl === "same") {
-                                $project.thumbUrl = $project.imageUrl;
-                            }
+                        // if thumb field is 'local'
+                        if($project.thumbUrl === "local") {
+                            var thumbSrc = ($project.imageUrl.replace(/^.*(\\|\/|\:)/, ''));
+                            $project.thumbUrl = dirThumbs + thumbSrc.substr(0, thumbSrc.lastIndexOf(".")) + ".jpg";
+                        }
 
-                            // if thumb field is 'local'
-                            if($project.thumbUrl === "local") {
-                                var thumbSrc = ($project.imageUrl.replace(/^.*(\\|\/|\:)/, ''));
-                                $project.thumbUrl = dirThumbs + thumbSrc.substr(0, thumbSrc.lastIndexOf(".")) + ".jpg";
-                            }
-
-                            // add to list
-                            projectList.push($project);
-                        });
+                        // add to list
+                        projectList.push($project);
                     });
 
                     // projects = projectList;
